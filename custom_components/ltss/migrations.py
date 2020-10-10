e6c32f3bad52d391b5e0ca63fc0011973494e424
freol35241 <freol@outlook.com>
2020-10-10 12:00:32 +0200

    Adding composite index on entity_id and time. Removing no longer needed index on solely entity_id.

--- custom_components/ltss/migrations.py
+++ custom_components/ltss/migrations.py
@@ -2,7 +2,7 @@ import logging
 
 from sqlalchemy import inspect, text, Text
 
-from .models import LTSS, LTSS_attributes_index
+from .models import LTSS, LTSS_attributes_index, LTSS_entityid_time_composite_index
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -10,24 +10,33 @@ def check_and_migrate(engine):
     
     #Inspect the DB
     iengine = inspect(engine)
+    columns = iengine.get_columns(LTSS.__tablename__)
+    indexes = iengine.get_indexes(LTSS.__tablename__)
+    
+    def index_exists(index_name):
+        matches = [idx for idx in indexes if idx["name"] == index_name]
+        return True if matches else False
     
     # Attributes column Text -> JSONB
-    columns = iengine.get_columns(LTSS.__tablename__)
     attributes_column = next(col for col in columns if col["name"] == 'attributes')
-    
     if isinstance(attributes_column['type'], Text):
         _LOGGER.warning('Migrating you LTSS table to the latest schema, this might take a couple of minutes!')
         migrate_attributes_text_to_jsonb(engine)
         _LOGGER.info('Migration completed successfully!')
         
-        
     # Attributes Index?
-    indexes = iengine.get_indexes(LTSS.__tablename__)
-    
-    if not [idx for idx in indexes if idx["name"] == 'ltss_attributes_idx']:
+    if not index_exists('ltss_attributes_idx'):
         _LOGGER.warning('Creating an index for the attributes column, this might take a couple of minutes!')
         create_attributes_index(engine)
         _LOGGER.info('Index created successfully!')
+        
+    # entity_id and time composite Index?
+    if not index_exists('ltss_entityid_time_composite_idx'):
+        _LOGGER.warning('Creating a composite index over entity_id and time columns, this might take a couple of minutes!')
+        create_entityid_time_index(engine)
+        _LOGGER.info('Index created successfully!')
+        _LOGGER.warning('Index on entity_id no longer needed, dropping...')
+        drop_entityid_index(engine)
 
 def migrate_attributes_text_to_jsonb(engine):
     
@@ -43,3 +52,18 @@ def create_attributes_index(engine):
         
         _LOGGER.info("Creating GIN index on the attributes column")
         LTSS_attributes_index.create(bind=engine)
+        
+def create_entityid_time_index(engine):
+        
+        _LOGGER.info("Creating composite index over entity_id and time columns")
+        LTSS_entityid_time_composite_index.create(bind=engine)
+        
+def drop_entityid_index(engine):
+    
+    with engine.connect() as con:
+        
+        _LOGGER.info("Dropping index on entity_id column if it exists")
+        con.execute(text(
+            f"""DROP INDEX IF EXISTS ix_ltss_entity_id;"""
+        ).execution_options(autocommit=True))
+    
