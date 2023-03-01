35a29bfcdaae7754ddb80bcd6eee1cd2a32b6fac
freol35241 <freol@outlook.com>
2023-03-01 21:15:00 +0100

    [WIP] Repository structure rework (#48)
    * Initial devcontainer structure
    
    * Removing info.md in favor of rendering README.md directly to avoid text duplicaiton.
    
    * Adding docker inside of the devcontainer to allow for running the test suite.
    
    * Update python-app.yml
    
    * Explicitly passing arguments to pytest
    
    * Fixing ci test setup

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -12,23 +12,29 @@ class TestDBSetup:
     @pytest.fixture(autouse=True)
     def reset_LTSS(self):
         # needed because the tests might change the underlying SQLAlchemy models and that needs to be reset
-        cols = [col for col in LTSS.__table__._columns if col.name == 'location']
+        cols = [col for col in LTSS.__table__._columns if col.name == "location"]
         if len(cols):
             LTSS.__table__._columns.remove(cols[0])
 
     @staticmethod
     def db_container(image):
         client = docker.from_env()
-        container = client.containers.run(image, ports={5432: None},
-                                          environment=["POSTGRES_HOST_AUTH_METHOD=trust"], detach=True,
-                                          auto_remove=True
-                                          )
+        container = client.containers.run(
+            image,
+            ports={5432: None},
+            environment=["POSTGRES_HOST_AUTH_METHOD=trust"],
+            detach=True,
+            auto_remove=True,
+        )
 
         start_time = time.time()
 
         # because a newly created postgresql container (with empty data directory) will immediately restart,
         # we have to wait for the second message to be sure postgres is ready for connections
-        while container.logs().count(b'database system is ready to accept connections') != 2:
+        while (
+            container.logs().count(b"database system is ready to accept connections")
+            != 2
+        ):
             if start_time + 30 <= time.time():  # timeout
                 raise RuntimeError("Container (or database) won't start.")
 
@@ -40,8 +46,13 @@ class TestDBSetup:
 
     @staticmethod
     def ltss_init_wrapper(container):
-        return LTSS_DB(None, 'postgresql://postgres@localhost:' + container.ports['5432/tcp'][0]['HostPort'],
-                       123, lambda x: False)
+        return LTSS_DB(
+            None,
+            "postgresql://postgres@localhost:"
+            + container.ports["5432/tcp"][0]["HostPort"],
+            123,
+            lambda x: False,
+        )
 
     def test_lite(self):
         container = self.db_container("postgres:latest")
@@ -56,9 +67,17 @@ class TestDBSetup:
         finally:
             container.stop()
 
-    @pytest.mark.parametrize("version",
-                             ["latest-pg9.6", "latest-pg10", "latest-pg11", "latest-pg12", "latest-pg13", "latest-pg14"]
-                             )
+    @pytest.mark.parametrize(
+        "version",
+        [
+            "latest-pg9.6",
+            "latest-pg10",
+            "latest-pg11",
+            "latest-pg12",
+            "latest-pg13",
+            "latest-pg14",
+        ],
+    )
     def test_timescaledb(self, version):
         container = self.db_container(f"timescale/timescaledb:{version}")
         try:
@@ -88,12 +107,16 @@ class TestDBSetup:
 
     @staticmethod
     def _is_hypertable(con):
-        timescaledb_version = con.execute(text("SELECT installed_version "
-                                          "FROM pg_available_extensions "
-                                          "WHERE name = 'timescaledb'")).scalar()
+        timescaledb_version = con.execute(
+            text(
+                "SELECT installed_version "
+                "FROM pg_available_extensions "
+                "WHERE name = 'timescaledb'"
+            )
+        ).scalar()
 
         # timescaledb's table/column name changed with v2
-        if int(timescaledb_version.split('.')[0]) >= 2:
+        if int(timescaledb_version.split(".")[0]) >= 2:
             query = f"SELECT 1 FROM timescaledb_information.hypertables "
             f"WHERE hypertable_name = '{LTSS.__tablename__}'"
         else:
@@ -104,13 +127,27 @@ class TestDBSetup:
 
     @staticmethod
     def _has_columns(con):
-        return 5 <= con.execute(text(f"SELECT COLUMN_NAME\
+        return (
+            5
+            <= con.execute(
+                text(
+                    f"SELECT COLUMN_NAME\
         FROM information_schema.columns\
-        WHERE table_name = '{LTSS.__tablename__}'")).rowcount
+        WHERE table_name = '{LTSS.__tablename__}'"
+                )
+            ).rowcount
+        )
 
     @staticmethod
     def _has_location_column(con):
-        return 1 == con.execute(text(f"SELECT *\
+        return (
+            1
+            == con.execute(
+                text(
+                    f"SELECT *\
         FROM information_schema.columns\
         WHERE table_name = '{LTSS.__tablename__}'\
-        AND COLUMN_NAME = 'location'")).rowcount
+        AND COLUMN_NAME = 'location'"
+                )
+            ).rowcount
+        )
