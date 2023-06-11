aeb1f9533885452b11eedc579f1f99c4f521fb5a
nwerter <noudwerter@gmail.com>
2023-06-11 10:55:27 +0200

    remove id column + 7 day default

--- custom_components/ltss/migrations.py
+++ custom_components/ltss/migrations.py
@@ -46,6 +46,25 @@ def check_and_migrate(engine):
             _LOGGER.warning("Index on entity_id no longer needed, dropping...")
             drop_entityid_index(engine)
 
+    # id column?
+    if any(col["name"] == "id" for col in columns):
+        _LOGGER.warning(
+            "Migrating you LTSS table to the latest schema, this might take a couple of minutes!"
+        )
+        with engine.begin() as con:
+            con.execute(text(
+                    f"""ALTER TABLE {LTSS.__tablename__}
+                        DROP CONSTRAINT {LTSS.__tablename__}_pkey CASCADE,
+                        ADD PRIMARY KEY(time,entity_id);"""
+                )    
+            )
+            con.execute(text(
+                    f"""ALTER TABLE {LTSS.__tablename__}
+                        DROP COLUMN id"""
+                )    
+            )
+            con.commit()
+        _LOGGER.info('Migration completed successfully!')
 
 def migrate_attributes_text_to_jsonb(engine):
     with engine.connect() as con:
